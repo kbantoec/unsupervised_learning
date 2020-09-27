9a01bc1f0971eeb96c1a0a2a099928dc8dbcee0a
Kai Banto Eckert <y.banto@outlook.com>
2020-09-27 22:54:38 +0200

    Created config file

---
+++ config.py
@@ -0,0 +1,9 @@
+paths: dict = {'seeds': 'data/grains/seeds.csv',
+               'seeds-width-vs-length': 'data/grains/seeds-width-vs-length.csv',
+               'artists': 'data/Musical artists/artists.csv',
+               'scrobbler-small-sample': 'data/Musical artists/scrobbler-small-sample.csv',
+               'wikipedia-vectors': 'data/Wikipedia articles/wikipedia-vectors.csv',
+               'wikipedia-vocabulary': 'data/Wikipedia articles/wikipedia-vocabulary-utf8.csv',
+               'stocks': 'data/company-stock-movements-2010-2015-incl.csv',
+               'eurovision': 'data/eurovision-2016.csv',
+               'wine': 'data/wine.csv'}
